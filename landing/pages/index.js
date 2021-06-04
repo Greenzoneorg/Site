8ed18e851cd559298e804f40afd0e22ba63d0f78
arav <arav.1tb@gmail.com>
2021-06-04 00:18:13 -0400

    error

--- landing/pages/index.js
+++ landing/pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+         Learn <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Find if items, are recyable or not{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -44,6 +44,8 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
+
+            
             <h2>Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
