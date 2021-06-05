61f415f67f6f9366d64df2d2377760133a1e8155
arav <arav.1tb@gmail.com>
2021-06-04 23:18:27 -0400

    work on about

--- pages/_abouts.js
+++ pages/_abouts.js
@@ -3,6 +3,10 @@ import Image from 'next/image'
 
 export default function Green() {
     return ( 
-        <h1 className="mb-6 text-2xl font-semibold tracking-tighter text-black sm:text-5xl title-font"> Greenzone</h1>
+
+        <div>
+        <h1 className="mb-6 text-2xl font-semibold tracking-tighter text-black sm:text-5xl title-font"> About</h1>
+        <p>We're built by teenagers</p> 
+        </div>
     )
 }
