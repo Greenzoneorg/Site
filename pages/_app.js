1cb8c5753139e94ca051aadb487f825ada633005
arav <arav.1tb@gmail.com>
2021-06-06 14:44:33 -0400

    yarn

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,7 @@ import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-<<<<<<< HEAD
-  return (
-    
-      <Component {...pageProps} />
-
-  );
-=======
   return <Component {...pageProps} />;
->>>>>>> 3d155e6c7bdeef1ca79264ac3e2f482d2ace471b
 }
 
 export default MyApp;
